08e4ac647f0549b1e0a478f671b0278237aa5792
Ive Tourinho <IveTou@users.noreply.github.com>
2019-05-06 23:07:58 -0300

    Corrections at API KEY string

--- constants/apis.js
+++ constants/apis.js
@@ -2,4 +2,4 @@ export const NYT_API = 'https://api.nytimes.com/svc/movies/v2';
 export const NYT_REVIEWS_ALL = '/reviews/all.json?';
 export const NYT_REVIEWS_PICKS = '/reviews/picks.json?';
 
-export const NYT_ACCESS_TOKEN = 'api-key=iQbOQ7IWMReXiFiHs3JNwh3khwMwhs1b';
+export const NYT_ACCESS_TOKEN = 'iQbOQ7IWMReXiFiHs3JNwh3khwMwhs1b';
